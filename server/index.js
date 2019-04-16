3da0cceb93b41f8c9514ff7ae97f6d3022bbb3f6
Md. Abu Taher <entrptaher@gmail.com>
2019-04-16 17:04:12 +0600

    Client and server separated

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const app = express();
 const port = 4000;
 const path = require('path');
 
-app.use('/static', express.static(path.join(__dirname, 'client/build')));
+app.use('/static', express.static(path.join(__dirname, '/../client/build')));
 
 app.get('/api', (req, res) => res.send({ echo: 'Hello World!' }));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
